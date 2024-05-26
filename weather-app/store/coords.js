c660804195a237b095f9a1a61c6078a2af799042
Ice_4Ever <arnaudorban.pro@gmail.com>
2024-05-26 21:45:37 +0200

    remove: store

--- weather-app/store/coords.js
+++
@@ -1,9 +0,0 @@
-import { useGeolocation } from '@vueuse/core'
-const { coords } = useGeolocation()
-
-export const CoordsStore = reactive({
-    latitude: coords.value.latitude,
-    longitude: coords.value.longitude,
-    currentLatitude: coords.value.latitude,
-    currentLongitude: coords.value.longitude,
-});
