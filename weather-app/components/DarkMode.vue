0b92950d8d618970f0975c50ee5bd5f3ab5680eb
Ice_4Ever <arnaudorban.pro@gmail.com>
2024-05-27 16:28:11 +0200

    fix dark mode

--- weather-app/components/DarkMode.vue
+++ weather-app/components/DarkMode.vue
@@ -1,11 +1,11 @@
 <template>
     <div class=" z-10 flex flex-row items-center justify-start p-4 gap-2">
-        <Icon name="material-symbols:light-mode-outline" class="text-black" size="25" />
+        <Icon name="material-symbols:light-mode-outline" class="text-black dark:text-white" size="25" />
       <label class="inline-flex items-center cursor-pointer">
         <input type="checkbox" class="sr-only peer" @click="toggleTheme" :checked="colorMode.preference === 'dark'">
         <div class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4  rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-gray-700"></div>
       </label>
-      <Icon name="tdesign:mode-dark" class="text-black" size="25" />
+      <Icon name="tdesign:mode-dark" class="text-black dark:text-white" size="25" />
     </div>
   </template>
 
