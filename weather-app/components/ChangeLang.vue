e93f3b91371e768b2d0dd00fe8e29e69787901ac
Ice_4Ever <arnaudorban.pro@gmail.com>
2024-05-27 19:12:44 +0200

    fix: error synthaxe

--- weather-app/components/ChangeLang.vue
+++ weather-app/components/ChangeLang.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex flex-row items-center justify-start z-10">
       <form>
-            <select v-model="locale" class="form-select bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"">
+            <select v-model="locale" class="form-select bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
               <option value="en">English</option>
               <option value="fr">Français</option>
             </select>  
@@ -10,6 +10,6 @@
   </template>
   
   <script setup>  
-  const { locale } = useI18n()
+  const { locale } = useI18n();
   </script>
   
