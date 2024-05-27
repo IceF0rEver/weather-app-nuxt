4647a107d81c47ea63562b9452205d5d8db5d3e5
Ice_4Ever <arnaudorban.pro@gmail.com>
2024-05-27 14:31:25 +0200

    feat: prochaines heures

--- weather-app/components/Hour.vue
+++ weather-app/components/Hour.vue
@@ -1,12 +1,3 @@
-<script setup>
-// import { defineProps } from 'vue';
-
-const props = defineProps({
-  processedResult: Object,
-});
-
-</script>
-
 <template>
   <div class="text-center">
     <p class="text-lg">
@@ -16,8 +7,19 @@ const props = defineProps({
     </p>
     <div class="w-10 h-10 bg-blue-300 rounded-full mx-auto my-2">
       <img :src="'https://openweathermap.org/img/wn/' + processedResult.weather[0].icon + '@2x.png'">
-
     </div>
-    <p class="text-lg">{{ processedResult.main.temp.toFixed(1) }}°C</p>
+    <p class="text-lg">{{ processedResult.main.temp.toFixed(0) }}°C</p>
   </div>
 </template>
+
+<script setup lang="ts">
+interface ProcessedResult {
+  dt: any;
+  weather: Array<{ icon: string }>;
+  main: { temp: number };
+}
+
+const props = defineProps<{
+  processedResult: ProcessedResult;
+}>();
+</script>
--- weather-app/components/Hourly.vue
+++ weather-app/components/Hourly.vue
@@ -1,70 +1,49 @@
 <script setup>
-// import Hour from "./Hour.vue";
-// import { ref, onMounted } from 'vue';
-// import { ForecastsStore } from "../stores/forecasts.js";
+import { GeoLocation } from "~/composables/geolocation.js";
 
-const results = ref(null);
-const processedResults = ref(null);
+const { $forecast } = useNuxtApp();
 
-// onMounted(async () => {
-//   try {
-//     results.value = await ForecastsStore.getByCoord();
-//     processResults();
-//   } catch (error) {
-//     console.error(error.message);
-//   }
-// });
+const coords = await GeoLocation.getCoords();
+const results = await $forecast.getByCoord(coords.latitude, coords.longitude);
 
-const processResults = () => {
-  if (!results.value) return;
+const processedResults = ref([]);
 
-  // Initialisation du tableau traité
-  processedResults.value = [];
-
-  // Parcours des données de l'API
+if (results.value) {
   for (let i = 0; i < results.value.list.length; i++) {
-    const currentData = results.value.list[i];
-    const nextData = results.value.list[i + 1];
+  const currentData = results.value.list[i];
+  const nextData = results.value.list[i + 1];
+  processedResults.value.push(currentData);
+  if (nextData) {
+    const numberOfIntervals = 3;
 
-    // Ajout des données actuelles au tableau traité
-    processedResults.value.push(currentData);
-
-    // Calcul des moyennes et ajout des données intermédiaires
-    if (nextData) {
-      const numberOfIntervals = 3; // Nombre d'intervalles entre deux données
-
-      for (let j = 1; j < numberOfIntervals; j++) {
-        const interpolatedData = {
-          dt:
-            currentData.dt +
-            (j * (nextData.dt - currentData.dt)) / numberOfIntervals,
-          main: {
-            temp: parseFloat(
-              (
-                currentData.main.temp +
-                (j * (nextData.main.temp - currentData.main.temp)) /
-                  numberOfIntervals
-              ).toFixed(2)
-            ),
-          },
-          weather: [...currentData.weather], // Vous pouvez également interpoler d'autres propriétés
-        };
-
-        processedResults.value.push(interpolatedData);
-
-        // Limite à 12 résultats
-        if (processedResults.value.length >= 12) {
-          break;
-        }
+    for (let j = 1; j < numberOfIntervals; j++) {
+      const interpolatedData = {
+        dt:
+          currentData.dt +
+          (j * (nextData.dt - currentData.dt)) / numberOfIntervals,
+        main: {
+          temp: parseFloat(
+            (
+              currentData.main.temp +
+              (j * (nextData.main.temp - currentData.main.temp)) /
+                numberOfIntervals
+            ).toFixed(2)
+          ),
+        },
+        weather: [...currentData.weather],
+      };
+      processedResults.value.push(interpolatedData);
+      if (processedResults.value.length >= 12) {
+        break;
       }
     }
-
-    // Limite à 12 résultats
-    if (processedResults.value.length >= 12) {
-      break;
-    }
   }
-};
+  if (processedResults.value.length >= 12) {
+    break;
+  }
+}  
+console.log(processedResults);
+}
 </script>
 
 <template>
